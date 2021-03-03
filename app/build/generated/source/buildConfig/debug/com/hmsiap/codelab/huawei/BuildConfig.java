a79a355de49e7deb6b53d3b3ad3f363352a3f1b0
jose vera <jose.vera@huawei.com>
2021-03-03 18:39:07 -0500

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/hmsiap/codelab/huawei/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hmsiap.codelab.huawei;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hmsiap.codelab.huawei";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 40000300;
+  public static final String VERSION_NAME = "4.0.0.300";
+}
